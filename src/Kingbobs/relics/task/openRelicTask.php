b7c9dca61937db324599b18d0fc4455ec600a4d1
KingBob <52259912+Kingbobs@users.noreply.github.com>
2023-06-24 19:28:10 -0600

    Update openRelicTask.php

--- src/Kingbobs/relics/task/openRelicTask.php
+++ src/Kingbobs/relics/task/openRelicTask.php
@@ -3,7 +3,6 @@
 namespace Kingbobs\relics\task;
 
 use pocketmine\command\ConsoleCommandSender;
-use pocketmine\level\particle\ExplodeParticle;
 use pocketmine\level\particle\CriticalParticle;
 use pocketmine\math\Vector3;
 use pocketmine\Player;
@@ -12,34 +11,34 @@ use pocketmine\Server;
 use function mt_rand;
 use function str_replace;
 
-class openRelicTask extends Task{
+class OpenRelicTask extends Task {
 
-	public $x;
-	public $y;
-	public $z;
-	public $player;
-	public $type;
-	public $plugin;
+    public $x;
+    public $y;
+    public $z;
+    public $player;
+    public $type;
+    public $plugin;
 
-	public function __construct($x, $y, $z, Player $player, $type, $plugin){
-		$this->x = $x;
-		$this->y = $y;
-		$this->z = $z;
-		$this->player = $player;
-		$this->type = $type;
-		$this->plugin = $plugin;
-	}
+    public function __construct($x, $y, $z, Player $player, $type, $plugin) {
+        $this->x = $x;
+        $this->y = $y;
+        $this->z = $z;
+        $this->player = $player;
+        $this->type = $type;
+        $this->plugin = $plugin;
+    }
 
-	/**
-	 * @inheritDoc
-	 */
-	public function onRun(int $currentTick){
-		$pos = new Vector3($this->x, $this->y, $this->z);
-		$this->player->getLevel()->addParticle(new CriticalParticle($pos));
-		$input = $this->plugin->config[$this->type]["Commands"];
-		$command =  $input[mt_rand(0, count($input) - 1)];
-		$cmd = str_replace("{PLAYER}", $this->player->getName(), $command);
-		Server::getInstance()->dispatchCommand(new ConsoleCommandSender, $cmd);
-		$this->plugin->opening[$this->player->getName()] = true;
-	}
+    /**
+     * @inheritDoc
+     */
+    public function onRun(int $currentTick) {
+        $pos = new Vector3($this->x, $this->y, $this->z);
+        $this->player->getLevel()->addParticle(new CriticalParticle($pos));
+        $input = $this->plugin->config[$this->type]["Commands"];
+        $command = $input[mt_rand(0, count($input) - 1)];
+        $cmd = str_replace("{PLAYER}", $this->player->getName(), $command);
+        Server::getInstance()->dispatchCommand(new ConsoleCommandSender(), $cmd);
+        $this->plugin->opening[$this->player->getName()] = false; // Corrected the value to false
+    }
 }
